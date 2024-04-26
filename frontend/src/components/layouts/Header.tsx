2175fa55525354787b3f1499016823060aaf1c43
신시은 <nssu422@naver.com>
2024-04-26 14:58:43 +0900

    [fe][Init] #S10P31D103-91 #done fontend 초기 환경 세팅
    header, home page 1차 완료

---
+++ frontend/src/components/layouts/Header.tsx
@@ -0,0 +1,85 @@
+import { useState, useEffect, useRef } from "react";
+import { useNavigate } from "react-router-dom";
+import headerStyle from "./css/Header.module.css";
+
+interface MenuItem {
+    name: string;
+    path: string;
+}
+
+interface MenuItems {
+    [key: string]: MenuItem[] | never[];
+}
+
+const menuItems: MenuItems = {
+    Docs: [
+        { name: "Hello Photo!", path: "/docs/product1" },
+        { name: "Dictionary", path: "/docs/product2" },
+    ],
+    Studio: [], // Studio에는 메뉴 아이템이 없으므로 never[] 타입을 유지
+    Community: [
+        { name: "Gallery", path: "/community/topic1" },
+        { name: "Exhibition", path: "/community/topic2" },
+        { name: "QnA", path: "/community/topic3" },
+    ],
+};
+
+const Header = () => {
+    const [openMenu, setOpenMenu] = useState<string>("");
+    const navigate = useNavigate();
+    const headerRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        // 외부 클릭 감지 함수
+        function handleClickOutside(event: MouseEvent) {
+            if (headerRef.current && !headerRef.current.contains(event.target as Node)) {
+                setOpenMenu("");
+            }
+        }
+
+        // 문서 전체에 클릭 이벤트 리스너 추가
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            // 컴포넌트 언마운트 시 이벤트 리스너 제거
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, [headerRef]);
+
+    return (
+        <div ref={headerRef} className="header">
+            <header className="h-[60px] bg-black text-white font-bookkMyungjoBold p-3 flex justify-between items-center">
+                <button className={headerStyle.logo}>PhotoIs</button>
+                <div className="rightSection flex">
+                    {Object.keys(menuItems).map((item) => (
+                        <div className="relative" onClick={() => setOpenMenu(openMenu === item ? "" : item)} key={item}>
+                            <button className={headerStyle.btn}>{item}</button>
+                            {openMenu === item && menuItems[item].length > 0 && (
+                                <div
+                                    className={`absolute left-1/2 transform -translate-x-1/2 top-[43px] mt-px w-40 bg-white text-black shadow-md ${headerStyle.dropdown}`}
+                                >
+                                    <ul>
+                                        {menuItems[item].map((subItem: MenuItem) => (
+                                            <li
+                                                key={subItem.name}
+                                                className="px-4 py-2 hover:bg-[#4C4C4C] hover:text-white"
+                                                onClick={(e) => {
+                                                    e.stopPropagation();
+                                                    navigate(subItem.path);
+                                                }}
+                                            >
+                                                {subItem.name}
+                                            </li>
+                                        ))}
+                                    </ul>
+                                </div>
+                            )}
+                        </div>
+                    ))}
+                    <button className={headerStyle["btn-signin"]}>SIGN IN</button>
+                </div>
+            </header>
+        </div>
+    );
+};
+
+export default Header;
