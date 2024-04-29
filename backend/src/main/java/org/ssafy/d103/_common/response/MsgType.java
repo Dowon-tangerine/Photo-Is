004af2a7ad21f559012cb91192e1c77a25107846
sehyun <germs1020@gmail.com>
2024-04-29 17:40:51 +0900

    [be][Feat] #S10P31D103-153 #Done 회원가입 API 구현 1. MemberController, PostMemberRequest, MemberRepository, MemberService 구현 2. MsgType, ErrorType에 Members 관련 에러 메시지 추가

--- backend/src/main/java/org/ssafy/d103/_common/response/MsgType.java
+++ backend/src/main/java/org/ssafy/d103/_common/response/MsgType.java
@@ -6,6 +6,9 @@ import lombok.Getter;
 public enum MsgType {
 
     TEST_MSG("테스트 메시지"),
+
+    //********************************[ Members ]********************************
+    SIGNUP_SUCCESSFULLY("회원가입에 성공하였습니다."),
     ;
 
     private final String msg;
---
+++ backend/src/main/java/org/ssafy/d103/members/repository/MemberRepository.java
@@ -0,0 +1,14 @@
+package org.ssafy.d103.members.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.ssafy.d103.members.entity.Members;
+
+import java.util.Optional;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Members, Long> {
+
+    Optional<Members> findMembersByEmailAndDeletedAtIsNull(String email);
+
+}
