01741975e56ab4fb7bc045f4d96b2a450f5ccf9b
sehyun <germs1020@gmail.com>
2024-04-29 10:17:40 +0900

    [be][Comment] #S10P31D103-60 #in-progress 프로젝트 세팅 주석 추가 1. build.gradle 주석 추가 2. ResponseUtils 주석 추가

--- backend/src/main/java/org/ssafy/d103/_common/response/ResponseUtils.java
+++ backend/src/main/java/org/ssafy/d103/_common/response/ResponseUtils.java
@@ -2,6 +2,9 @@ package org.ssafy.d103._common.response;
 
 public class ResponseUtils {
 
+    /**
+     * 돌려줄 데이터가 있을 때 사용
+     */
     public static <T> ApiResponseDto<T> ok (T data, MsgType msg) {
         return ApiResponseDto.<T> builder()
                 .data(data)
@@ -9,6 +12,9 @@ public class ResponseUtils {
                 .build();
     }
 
+    /**
+     * 돌려줄 데이터가 없을 때 사용
+     */
     public static ApiResponseDto<Void> ok (MsgType msg) {
         return ApiResponseDto.<Void> builder()
                 .msg(msg.getMsg())
