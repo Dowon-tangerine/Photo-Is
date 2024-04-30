228b697fca3e28084c5f426e768fdbf60c086871
rlarbfl0702 <rlarbfl0702@naver.com>
2024-04-30 17:53:17 +0900

    [fe][Fix] #S10P31D103-118 #in-progress 갤러리 화면 구현 1. 검색 부분 구현 (디자인)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainLayout from "./components/layouts/MainLayout";
 import Home from "./components/home/Home";
 import StudioEnterPage from "./components/studio/StudioEnterPage";
+import Gallery from "./components/gallery/Gallery";
+
 export default function App() {
     return (
         <BrowserRouter>
@@ -9,6 +11,7 @@ export default function App() {
                 <Route element={<MainLayout />}>
                     <Route path="/" element={<Home />} />
                     <Route path="/studio-enter" element={<StudioEnterPage />} />
+                    <Route path="/community/gallery" element={<Gallery />}/>
                 </Route>
             </Routes>
         </BrowserRouter>
