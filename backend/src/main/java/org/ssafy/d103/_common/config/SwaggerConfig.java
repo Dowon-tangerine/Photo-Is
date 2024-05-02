8be3ed1d59113cba6a40162bfb8c4cbda4fd58fe
sehyun <germs1020@gmail.com>
2024-05-02 14:45:13 +0900

    [be][Feat] Swagger 추가 1. SecurityConfig에 swagger 관련 url 허용 2. SwaggerConfig 클래스 추가 3. build.gradle swagger 의존성 추가 4. application.yml에 swagger 관련 세팅 추가

---
+++ backend/src/main/java/org/ssafy/d103/_common/config/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package org.ssafy.d103._common.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import lombok.RequiredArgsConstructor;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "PhotoIs API Docs",
+                description = "PhotoIs의 API 문서입니다.",
+                version = "v1"
+        )
+)
+@Configuration
+@RequiredArgsConstructor
+public class SwaggerConfig {
+
+
+}
