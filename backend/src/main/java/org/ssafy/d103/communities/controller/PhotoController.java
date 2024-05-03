241de3a686865120a2e78cf1995d6f9720d7b387
cyeon1118 <cyeon1118@naver.com>
2024-05-03 14:41:15 +0900

    [be][Feat] #S10P31D103-110 #in-progress 사진 업로드 API 구현 및 Authentication 적용

--- backend/src/main/java/org/ssafy/d103/communities/controller/PhotoController.java
+++ backend/src/main/java/org/ssafy/d103/communities/controller/PhotoController.java
@@ -1,8 +1,9 @@
 package org.ssafy.d103.communities.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
@@ -13,10 +14,10 @@ import org.ssafy.d103._common.response.MsgType;
 import org.ssafy.d103._common.response.ResponseUtils;
 import org.ssafy.d103.communities.dto.request.PostUploadPhotoRequest;
 import org.ssafy.d103.communities.service.PhotoService;
-import org.ssafy.d103.members.service.UserDetailsImpl;
 
 import java.io.IOException;
 
+@Tag(name = "communities", description = "Communities API")
 @Slf4j
 @RestController
 @RequestMapping("/api/photos")
@@ -26,7 +27,7 @@ public class PhotoController {
     private final PhotoService photoService;
 
     @PostMapping("/upload")
-    public ApiResponseDto<?> postUploadPhoto(@AuthenticationPrincipal UserDetailsImpl userDetails, @RequestPart(value = "photoInfo") PostUploadPhotoRequest postUploadPhotoRequest, @RequestPart(value = "photo", required = false) MultipartFile multipartFile) throws IOException {
-        return ResponseUtils.ok(photoService.uploadPhoto(userDetails, multipartFile, postUploadPhotoRequest), MsgType.PHOTO_UPLOAD_SUCCESSFULLY);
+    public ApiResponseDto<?> postUploadPhoto(Authentication authentication, @RequestPart(value = "photoInfo") PostUploadPhotoRequest postUploadPhotoRequest, @RequestPart(value = "photo", required = false) MultipartFile multipartFile) throws IOException {
+        return ResponseUtils.ok(photoService.uploadPhoto(authentication, multipartFile, postUploadPhotoRequest), MsgType.PHOTO_UPLOAD_SUCCESSFULLY);
     }
 }
