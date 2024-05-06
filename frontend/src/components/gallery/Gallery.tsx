a36c5fe659115beedf459aa7275d20b95b5a22dd
rlarbfl0702 <rlarbfl0702@naver.com>
2024-05-06 21:25:21 +0900

    [fe][Fix] #S10P31D103-118 #in-progress 갤러리 화면 구현 1. 사진 목록 구현  (디자인) 2. 무한스크롤 구현

--- frontend/src/components/gallery/Gallery.tsx
+++ frontend/src/components/gallery/Gallery.tsx
@@ -312,6 +312,7 @@ const Gallery: React.FC = () => {
         setIsLoading(false);
     };
 
+    
     useEffect(() => {
         const API_URL = "https://api.thedogapi.com/v1/images/search?size=small&format=json&has_breeds=true&order=ASC&page=0&limit=10";
         axios.get(API_URL).then((res) => {
