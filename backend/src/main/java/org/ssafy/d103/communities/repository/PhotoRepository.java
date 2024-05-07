597aeebe825e2b3df536a4cfa0d970cee7a43839
cyeon1118 <cyeon1118@naver.com>
2024-05-07 13:17:03 +0900

    [be][Feat] #S10P31D103-165 #done AccessType에 따른 사진 목록 조회 API 구현

--- backend/src/main/java/org/ssafy/d103/communities/repository/PhotoRepository.java
+++ backend/src/main/java/org/ssafy/d103/communities/repository/PhotoRepository.java
@@ -2,9 +2,11 @@ package org.ssafy.d103.communities.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.ssafy.d103.communities.entity.photo.AccessType;
 import org.ssafy.d103.communities.entity.photo.Photo;
 import org.ssafy.d103.members.entity.Members;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -13,4 +15,7 @@ public interface PhotoRepository extends JpaRepository<Photo, Long> {
     Optional<Photo> findPhotoByIdAndMember(Long id, Members member);
 
     void deleteByIdAndMember(Long id, Members member);
+
+    List<Photo> findAllByMemberAndAccessType(Members member, AccessType accessType);
+
 }
