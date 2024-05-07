cfc138c6f1a3dafb38238b3c0f250968d155f650
신시은 <nssu422@naver.com>
2024-05-07 15:39:07 +0900

    [fe][Init] #S10P31D103-133 #in-progress 가상 스튜디오 깡통 공간 구현  뒤로가기 구현 완료 + 그림자 구현 중
    Former-commit-id: 5b50edee2b6647c88fbe219171e95dc2f8ee0470

---
+++ frontend/src/components/layouts/StudioLayout.tsx
@@ -0,0 +1,21 @@
+import { Outlet, useNavigate } from "react-router-dom";
+
+function StudioLayout() {
+    const navigate = useNavigate(); // useNavigate 훅을 사용하여 navigate 함수를 가져옵니다.
+
+    const goBack = () => {
+        navigate(-1); // navigate 함수에 -1을 전달하여 이전 페이지로 이동
+    };
+
+    return (
+        <div className="relative min-h-screen">
+            <button onClick={goBack} className=" flex items-center absolute top-5 left-5 z-50">
+                <img src="./imgs/rightArrow.png" alt="" className="w-[40px] mr-2" />
+                <p className="text-[25px] font-bookkMyungjoBold">BACK</p>
+            </button>{" "}
+            <Outlet />
+        </div>
+    );
+}
+
+export default StudioLayout;
