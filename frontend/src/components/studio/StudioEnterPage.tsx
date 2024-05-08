315e350d965b4149e095497737255e3ca494ea7e
신시은 <nssu422@naver.com>
2024-05-08 09:37:04 +0900

    [fe][Fix] #done 에러 수정 - 배포 에러 수정
    Former-commit-id: 1b643c6b913ed0d8a606f790bcbe923eed3905df

--- frontend/src/components/studio/StudioEnterPage.tsx
+++ frontend/src/components/studio/StudioEnterPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import tutorialPicture from "/imgs/tutorialBackImg.png";
 import landscapePicture from "/imgs/landScapeBackImg.png";
