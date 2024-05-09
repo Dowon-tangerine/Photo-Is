49752faf4a5f51f5f88bbfce019e1d1b0f33cb15
cyeon1118 <cyeon1118@naver.com>
2024-05-09 15:48:51 +0900

    [be][Feat] #S10P31D103-184 #done 질문 댓글 삭제 API 구현

--- backend/src/main/java/org/ssafy/d103/_common/response/MsgType.java
+++ backend/src/main/java/org/ssafy/d103/_common/response/MsgType.java
@@ -50,6 +50,7 @@ public enum MsgType {
     QUESTION_MODIFY_SUCCESSFULLY("질문 수정에 성공하였습니다."),
     QUESTION_DELETE_SUCCESSFULLY("질문 삭제에 성공하였습니다."),
     QUESTION_COMMENT_WRITE_SUCCESSFULLY("질문 댓글 작성에 성공하였습니다."),
+    QUESTION_COMMENT_DELETE_SUCCESSFULLY("질문 댓글 삭제에 성공하였습니다."),
     ;
 
     private final String msg;
