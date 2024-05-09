c81ad281a31259248742081967a9312b2cf83e2c
cyeon1118 <cyeon1118@naver.com>
2024-05-09 16:46:12 +0900

    [be][Feat] #S10P31D103-174 #done 질문 게시글 목록 카테고리별 조회 API 구현

---
+++ backend/src/main/java/org/ssafy/d103/communities/dto/question/response/GetQuestionListByCategoryResponse.java
@@ -0,0 +1,35 @@
+package org.ssafy.d103.communities.dto.question.response;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.ssafy.d103.communities.dto.question.PaginationDataDto;
+import org.ssafy.d103.communities.dto.question.QuestionDto;
+
+import java.util.List;
+
+@Getter
+public class GetQuestionListByCategoryResponse {
+
+    private String category;
+    private Integer totalCnt;
+    private List<QuestionDto> questionList;
+    private PaginationDataDto paginationDataDto;
+
+    @Builder
+    private GetQuestionListByCategoryResponse(String category, Integer totalCnt, List<QuestionDto> questionList, PaginationDataDto paginationDataDto) {
+        this.category = category;
+        this.totalCnt = totalCnt;
+        this.questionList = questionList;
+        this.paginationDataDto = paginationDataDto;
+    }
+
+    public static GetQuestionListByCategoryResponse of(String category, Integer totalCnt, List<QuestionDto> questionList, PaginationDataDto paginationDataDto) {
+        return builder()
+                .category(category)
+                .totalCnt(totalCnt)
+                .questionList(questionList)
+                .paginationDataDto(paginationDataDto)
+                .build();
+    }
+
+}
--- backend/src/main/java/org/ssafy/d103/communities/repository/question/QuestionRepository.java
+++ backend/src/main/java/org/ssafy/d103/communities/repository/question/QuestionRepository.java
@@ -1,7 +1,10 @@
 package org.ssafy.d103.communities.repository.question;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.ssafy.d103.communities.entity.question.Category;
 import org.ssafy.d103.communities.entity.question.Question;
 import org.ssafy.d103.members.entity.Members;
 
@@ -11,5 +14,7 @@ import java.util.Optional;
 public interface QuestionRepository extends JpaRepository<Question, Long> {
 
     Optional<Question> findQuestionByIdAndMember(Long id, Members member);
+
+    Page<Question> findAllByCategory(Category category, Pageable pageable);
 
 }
