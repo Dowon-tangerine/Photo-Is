fa163cabb089086f9dece5b07ba3d1b63f33cad1
박창준 <qmak01@naver.com>
2024-05-13 04:31:21 +0900

    [be][Feat] 챗봇 질문, 사진 비평 API 작성

---
+++ backend/src/main/java/org/ssafy/d103/chatbots/dto/ChatResponseDto.java
@@ -0,0 +1,11 @@
+package org.ssafy.d103.chatbots.dto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatResponseDto {
+    private String answer;
+}
