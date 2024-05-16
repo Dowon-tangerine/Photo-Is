19b54103abf85a7d4dcf9e30f937896da643ec8d
신시은 <nssu422@naver.com>
2024-05-16 10:39:40 +0900

    [fe][Feat] #S10P22D201-189 #in-progress iso 구현
    Former-commit-id: 26c8854653361f480df37607a281fe2d03e19a87 [formerly 8a4381a208b264adc05237d035f5570db2705e8c]
    Former-commit-id: ed224a6e7d6beb79fd43586e00fc956adf9c732a

--- frontend/src/components/studio/3Delement/spinner.tsx
+++ frontend/src/components/studio/3Delement/spinner.tsx
@@ -42,6 +42,18 @@ function Spinner() {
         loader.setMaterials(axisMaterial);
     });
 
+    const grassMaterial = useLoader(MTLLoader, "/models/Grass1.mtl");
+    grassMaterial.preload();
+    const grass = useLoader(OBJLoader, "/models/Grass1.obj", (loader) => {
+        loader.setMaterials(grassMaterial);
+    });
+
+    const mountMaterial = useLoader(MTLLoader, "/models/mount.mtl");
+    mountMaterial.preload();
+    const mount = useLoader(OBJLoader, "/models/mount.obj", (loader) => {
+        loader.setMaterials(mountMaterial);
+    });
+
     useEffect(() => {
         if (blade) {
             bladesRefs.current.push(blade);
@@ -49,15 +61,26 @@ function Spinner() {
             blade.position.y = 6.7;
             setModelLoaded(true); // 모델 로드 완료 상태 설정
         }
-    }, [blade]);
+
+        if (mount) {
+            mount.position.x = -440;
+            mount.position.y = -10;
+            mount.scale.set(100, 190, 200); // mount 크기를 2배로 설정
+            setModelLoaded(true); // 모델 로드 완료 상태 설정
+        }
+        if (grass) {
+            grass.scale.set(2, 0.5, 3); // mount 크기를 2배로 설정
+            setModelLoaded(true); // 모델 로드 완료 상태 설정
+            grass.position.x = -12;
+        }
+    }, [blade, mount, grass]);
 
     useFrame((state, delta) => {
-        state
-
+        state;
         if (modelLoaded) {
             bladesRefs.current.forEach((blade) => {
                 if (blade) {
-                    const rotationSpeed = 10; // 회전 속도 조절
+                    const rotationSpeed = 0; // 회전 속도 조절
                     const angle = rotationSpeed * delta; // 프레임 속도에 따른 회전 각도 계산
                     blade.rotation.x += angle; // X축을 기준으로 회전
                 }
@@ -65,17 +88,17 @@ function Spinner() {
         }
     });
     const { gl } = useThree();
-    console.log(gl);
 
     return (
         <>
             {/* <ambientLight intensity={1} /> */}
-            <directionalLight ref={lightRef} castShadow position={[0, 100, -50]} intensity={2} />
             <OrbitControls />
+            <directionalLight ref={lightRef} castShadow position={[0, 100, -50]} intensity={2} />
             <Sky distance={40000} sunPosition={[0, 0.1, 0]} inclination={0} azimuth={0.25} />
             {blade && <primitive object={blade} />}
             {axis && <primitive object={axis} />}
             {stick && <primitive object={stick} />}
+            {mount && <primitive object={mount} />}
             {/* {grass && <primitive object={grass} />} */}
         </>
     );
