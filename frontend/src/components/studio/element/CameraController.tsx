a186a4c32cbe0ea66b0b8405a5279fcf02264758
신시은 <nssu422@naver.com>
2024-05-16 10:39:40 +0900

    [fe][Feat] #S10P22D201-189 #in-progress iso 구현
    Former-commit-id: 1fc4c9849742429a01788915068727ac1cf9975a [formerly 7ca1840e192c2d4d8584ac7af29605db7eeccd2c]
    Former-commit-id: 078a7a18c5baedd033b6a177858ed99b99290916

---
+++ frontend/src/components/studio/element/CameraController.tsx
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+import { useThree, useFrame, extend } from "@react-three/fiber";
+import * as THREE from "three";
+import { useCameraStore } from "../store/useCameraStore";
+
+function CameraController() {
+    const { setFocusDistance } = useCameraStore(); // Zustand 스토어에서 exposure 값을 가져옴
+    const { gl, camera, scene } = useThree();
+    const spinnerPosition = new THREE.Vector3(0, 6.5, 0.5); // Spinner의 위치
+    useEffect(() => {
+        // 카메라에서 Spinner까지의 거리 계산
+        const distance = camera.position.distanceTo(spinnerPosition);
+        console.log(camera);
+        camera.position.set(11, 4, 0);
+        camera.far = 10000;
+        setFocusDistance(distance);
+    }, [gl, camera]);
+
+    useFrame(() => {
+        camera.lookAt(spinnerPosition); // 카메라가 항상 Spinner를 바라보도록 설정
+    });
+
+    return null;
+}
+
+export default CameraController;
