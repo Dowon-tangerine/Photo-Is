1d87d306140da76a9e6f59d0c3c2a10b66df2e97
sehyun <germs1020@gmail.com>
2024-05-18 00:09:04 +0900

    [fe][Fix] 스튜디오 입장 페이지 로딩 속도 개선
    Former-commit-id: b9a4d0f66e5bddd56b7c1a348cd0ab947569afd5 [formerly edf4831ec2a3837ffe512267f2f7b2c55b81634c]
    Former-commit-id: 9bfcf05fab80a1e77a2acb97a5268e99329de6c7

--- frontend/src/components/studio/StudioEnterPage.tsx
+++ frontend/src/components/studio/StudioEnterPage.tsx
@@ -1,8 +1,8 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import tutorialPicture from "/imgs/tutorialBackImg.png";
-import landscapePicture from "/imgs/landScapeBackImg.png";
-import portraitPicture from "/imgs/portraitBackImg.png";
+import tutorialPicture from "/imgs/tutorialBackImg.jpg";
+import landscapePicture from "/imgs/landScapeBackImg.jpg";
+import portraitPicture from "/imgs/portraitBackImg.jpg";
 import StudioStyle from "./css/Studio.module.css";
 
 function StudioEnterPage() {
