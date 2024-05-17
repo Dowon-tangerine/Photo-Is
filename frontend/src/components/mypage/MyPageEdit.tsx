f9e91db64adaa9eecc10c032b93903aaf1986e06
sehyun <germs1020@gmail.com>
2024-05-17 17:38:00 +0900

    [fe][Feat & Fix] 화면 수정 페이지 구현 완료 및 헤더 버그 수정
    Former-commit-id: 85a2a8a7ec8431dcbe458c65dbfadb14c1ca51ef [formerly 86496df2a4b5a5d35050b3eb08a482a91ace6c4a]
    Former-commit-id: 6fe24d59ad31e58f972610ecf32c7849d842d22e

--- frontend/src/components/mypage/MyPageEdit.tsx
+++ frontend/src/components/mypage/MyPageEdit.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import './css/MyPageEdit.module.css';
 import styles from './css/MyPage.module.css';
 import editStyles from './css/MyPageEdit.module.css';
-import { searchProfile } from '../../apis/memberApi';
+import { searchProfile, editBackgroundImg, editProfile } from '../../apis/memberApi';
 
 interface memberInfoInterface { 
 	memberId: number;
@@ -18,6 +18,7 @@ interface memberInfoInterface {
 	camera: string;
 	country: string;
 	city: string;
+	password: string;
 }
 
 const MyPageEdit: React.FC = () =>{
@@ -36,9 +37,17 @@ const MyPageEdit: React.FC = () =>{
 		camera: '',
 		country: '',
 		city: '',
+		password: '',
 	});
+	const [backgroundImg, setBackgroundImg] = useState<ArrayBuffer | string | null>(null);
+	const [profileImg, setProfileImg] = useState<ArrayBuffer | string | null>(null);
+	const [backgroundImgFile, setBackgroundImgFile] = useState<File | null>(null);
+	const [profileImgFile, setProfileImgFile] = useState<File | null>(null);
 
 	useEffect(()=>{
+		loadMemberInfo();
+	}, [])
+	const loadMemberInfo = ()=>{
 		const myId = localStorage.getItem('memberId');
 		if(myId != null){
 			searchProfile(Number(myId))
@@ -53,7 +62,7 @@ const MyPageEdit: React.FC = () =>{
 				setMemberInfo(res);
 			})
 		}
-	}, [])
+	}
 
 	const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>)=>{
 		const name = e.target.name;
@@ -61,22 +70,91 @@ const MyPageEdit: React.FC = () =>{
 			...memberInfo,
 			[name]: e.target.value
 		});
+	}
 
-		console.log(memberInfo)
+	const onSelectHandler = (e: React.ChangeEvent<HTMLSelectElement>)=>{
+		const name = e.target.name;
+		alert(name)
+		setMemberInfo({
+			...memberInfo,
+			[name]: e.target.value
+		});
+	}
+
+	const onFileChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, files } = e.target;
+    if (files && files[0]) {
+        if (name === 'background') {
+            setBackgroundImgFile(files[0]); // 파일 객체 저장
+            const reader = new FileReader();
+            reader.onload = () => setBackgroundImg(reader.result as string); // 미리보기 용
+            reader.readAsDataURL(files[0]);
+			const formData = new FormData();
+			formData.append('background', files[0]);
+			editBackgroundImg(formData)
+			.then(()=>{})
+        } else if (name === 'profile') {
+            setProfileImgFile(files[0]); // 파일 객체 저장
+            const reader = new FileReader();
+            reader.onload = () => setProfileImg(reader.result as string); // 미리보기 용
+            reader.readAsDataURL(files[0]);
+        }
+	  }
+	};
+
+	const onSubmitHandler = ()=>{
+		const formData = new FormData();
+		formData.append('photo', profileImgFile!);
+		const blob = new Blob([JSON.stringify(memberInfo)], {type: "application/json"});
+		formData.append("memberInfo", blob);
+
+		editProfile(formData)
+		.then(res=>{
+			if(res){
+				alert('정보 수정에 성공하였습니다!');
+			}
+			else{
+				alert('중복된 닉네임입니다.')
+			}
+		})
+		.catch(err=>console.log(err))
+		.finally(()=>{
+			loadMemberInfo();
+		})
 	}
 
 	return(
-		<div className={styles.main_container}style={{ border: '5px solid red'}}>
-				<div className={styles.mypage_info_container} style={{backgroundImage : `url(${memberInfo.backgroundUrl})`, backgroundSize : 'cover', height : '350px', position : 'relative', backgroundPosition : 'center'}}>
+		<div className={styles.main_container} >
+			<label htmlFor='background'>
+				<div style={{position: 'absolute', zIndex: '2', right: '2%', top: '25%', height:'50px', width: '50px', cursor:'pointer', background:'black',borderRadius: '200px',}}>
+					<img src='imgs/upload_icon.png'/>
+				</div>
+			</label>
+			<input type='file' name='background' id='background' style={{position: 'absolute'}} onChange={onFileChangeHandler}/>
+			<input type='file' name='profile' id='profile' style={{position: 'absolute'}} onChange={onFileChangeHandler}/>
+				<div className={styles.mypage_info_container} style={{backgroundImage : `url(${backgroundImg || memberInfo.backgroundUrl})`, backgroundSize : 'cover', height : '350px', position : 'relative', backgroundPosition : 'center'}}>
 						<div className={styles.info_container}>
-								<img src={memberInfo.profileUrl} alt='프로필' style={{marginTop: '-40px', width: '200px', height: '200px', margin: 'auto'}}></img>
-						 </div>
 						
-						 <table style={{background: 'white', width: '80%', marginTop: '70%', marginBottom: '100px'}}>
+						<div style={{margin: 'auto', position: 'relative'}}>
+							
+						<label htmlFor='profile'>
+							<div style={{display:'flex', alignItems:'center', justifyContent:'center', position: 'absolute', zIndex: '2', height:'50px', right: '0', bottom: '0', width: '50px', cursor:'pointer', background:'rgba(0, 0, 0, 0.8)', borderRadius: '200px'}}>
+								<img src='imgs/upload_icon.png' style={{width: 'auto', height: '40px'}}/>
+							</div>
+						</label>
+							<img src={profileImg as string || memberInfo.profileUrl} alt='프로필' style={{marginTop: '-40px', width: '200px', height: '200px', margin: 'auto', borderRadius: '200px', objectFit:'cover'}}></img>
+						</div>
+						 </div>
+						 <table style={{background: 'white', width: '80%', marginTop: '1200px', marginBottom: '100px'}}>
+							<caption><h1 className={editStyles.h1}>Edit</h1></caption>
 							<tbody>
 							<tr>
 								<th>Nickname</th>
 								<td><input name='nickname' value={memberInfo.nickname} onChange={onChangeHandler}/></td>
+							</tr>
+							<tr>
+								<th>Password</th>
+								<td><input name='password' value={memberInfo.password} onChange={onChangeHandler}/></td>
 							</tr>
 							<tr>
 								<th>Birthyear</th>
@@ -84,7 +162,14 @@ const MyPageEdit: React.FC = () =>{
 							</tr>
 							<tr>
 								<th>MyCamera</th>
-								<td><input name='camera' value={memberInfo.camera} onChange={onChangeHandler}/></td>
+								<td style={{paddingLeft: '15px'}}>
+									<select style={{height: '60px', outline: 'none'}} name='camera' onChange={onSelectHandler}>
+										<option value="FUJI">FUJI</option>
+										<option value="CANON">CANON</option>
+										<option value="NIKON">NIKON</option>
+										<option value="SONY">SONY</option>
+									</select>
+								</td>
 							</tr>
 							<tr>
 								<th>Useyear</th>
@@ -102,7 +187,7 @@ const MyPageEdit: React.FC = () =>{
 							
 						 </table>
 				</div>
-				<div className={editStyles.button}>Sumbit</div>
+				<div className={editStyles.button} onClick={onSubmitHandler}><p>Submit</p></div>
 		</div>
 	);
 }
