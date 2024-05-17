f949298638ba604cb26e0f890d9f4c2b9f8b3722
신시은 <nssu422@naver.com>
2024-05-17 15:47:12 +0900

    [fe][Feat] #S10P22D103-233 #in-progress 모달 구현 완료 아직 저장은 연결 안됨  사진 찍히는 효과 있으면 좋을 듯
    Former-commit-id: 61f98c71cc885dc3cb06e7d08353dcb327ae4f90 [formerly 81880944a082ca1c67e31edc49628200e9b34ea8]
    Former-commit-id: 2943c81b6adf8aab51ff197a0a792047bdc43f9e

---
+++ frontend/src/components/studio/element/Modal.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { useCameraStore } from "../store/useCameraStore";
+
+interface ModalProps {
+    setModalIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+    ImgUrl: string | null;
+}
+const Modal: React.FC<ModalProps> = ({ setModalIsOpen, ImgUrl }) => {
+    const { iso, shutterSpeed, aperture, exposure } = useCameraStore();
+
+    return (
+        <div className="background w-full h-full bg-black bg-opacity-75 flex justify-center items-center absolute top-0 left-0 z-50">
+            <img
+                onClick={() => setModalIsOpen(false)}
+                src="/imgs/cancel.png"
+                alt=""
+                className="close-modal top-2 absolute left-2 w-[70px]"
+            />
+            <div className="modal w-[950px] h-[450px] bg-white flex">
+                <div className="img-section h-full bg-gray-400 aspect-[3/2]">
+                    {ImgUrl && <img src={ImgUrl} alt="Captured" className="w-full h-full object-cover" />}
+                </div>
+                <div className="flex flex-col w-full">
+                    <div className="flex flex-col items-center font-bookkGothic flex-grow text-[15px]">
+                        <p className="text-[30px] font-bookkMyungjoBold mt-4">My Setting</p>
+                        <div className="flex justify-between w-full px-4 pt-8 ">
+                            <p>조리개 / Aperture </p>
+                            <p>F {aperture} </p>
+                        </div>
+                        <div className="flex justify-between w-full  px-4 pt-5">
+                            <p>셔터스피드 / SS </p>
+                            {shutterSpeed === 1 ? <p>1</p> : <p>1/{shutterSpeed}</p>}
+                        </div>
+                        <div className="flex justify-between w-full  px-4 pt-5">
+                            <p>감도 / ISO </p>
+                            <p>{iso} </p>
+                        </div>
+                        <div className="flex justify-between w-full  px-4 pt-5">
+                            <p>노출 / Exposure </p>
+                            <p>{exposure} EV</p>
+                        </div>
+                        <button className="bg-black text-white font-bookkGothicBold w-44 h-11 rounded-sm mt-auto mb-4">
+                            SAVE
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Modal;
