a9788e8fcd4df31e751756edd42ad28ab9e7e94b
신시은 <nssu422@naver.com>
2024-05-19 01:52:07 +0900

    [fe][Feat] #S10P22D103-233 #in-progress 사진 저장 완료시 모달 구현 완료
    Former-commit-id: 2ec8b63a23616ef9700fb99db9883e1831d5dcd3 [formerly e351c77b6aa1193e35e3ded9bbd00059437b0b18]
    Former-commit-id: c96e0a0bc18023beb6c86c0843a7e9a3e541f41c

--- frontend/src/components/studio/element/Modal.tsx
+++ frontend/src/components/studio/element/Modal.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { useCameraStore } from "../store/useCameraStore";
-import { uploadPhoto, PhotoInfo, UploadPhotoPayload } from "../../../apis/studioApi";
 import styles from "../../mypage/css/MyPage.module.css";
 import { uploadGalleryPhoto } from "../../../apis/photoApi";
 
@@ -112,125 +111,158 @@ const Modal: React.FC<ModalProps> = ({ setModalIsOpen, ImgUrl }) => {
         setIsUploadFinished(true);
     };
 
+    const handelModal = () => {
+        setModalIsOpen(false);
+        setIsUploadFinished(false);
+    };
+
     return (
         <div className="background w-full h-full bg-black bg-opacity-75 flex justify-center items-center absolute top-0 left-0 z-50">
             <img
-                onClick={() => setModalIsOpen(false)}
+                onClick={handelModal}
                 src="/imgs/cancel.png"
                 alt=""
                 className="close-modal top-2 absolute left-2 w-[70px]"
             />
-            <div className="modal w-[950px] h-[450px] bg-white flex">
-                <div className="img-section h-full bg-gray-400 aspect-[3/2]">
-                    {ImgUrl && <img src={ImgUrl} alt="Captured" className="w-full h-full object-cover" />}
+            {isUploadFinished ? (
+                <div className="flex-col modal w-[950px] h-[450px] bg-white flex justify-center items-center">
+                    <img src="/imgs/check_gif.gif" alt="체크" style={{ height: "100px", width: "100px" }}></img>
+                    <p className="text-[30px] font-bookkGothicBold">사진을 성공적으로 업로드하였습니다.</p>
+                    <div>
+                        <p className="text-[15px] font-bookkGothic pt-4">
+                            작은 노력이 모여 큰 성과를 이루게 합니다. <br></br>
+                        </p>
+                    </div>
+                    <p className="text-[15px] font-bookkGothic ">꾸준하게 사진을 업로드 해보세요</p>
+                    <div className="pt-12 w-full flex justify-center">
+                        <button
+                            onClick={handelModal}
+                            className="w-48 h-12 border font-bookkGothicBold border-black rounded-lg mx-2"
+                        >
+                            CONTINUE
+                        </button>
+                        <button className="w-48 h-12 border bg-black text-white font-bookkGothicBold rounded-lg mx-2">
+                            MY PAGE
+                        </button>
+                    </div>
                 </div>
+            ) : (
+                <>
+                    <div className="modal w-[950px] h-[450px] bg-white flex">
+                        <div className="img-section h-full bg-gray-400 aspect-[3/2]">
+                            {ImgUrl && <img src={ImgUrl} alt="Captured" className="w-full h-full object-cover" />}
+                        </div>
 
-                <div className="flex flex-col w-full">
-                    {uploadPhoto ? (
-                        <div className="flex flex-col items-center font-bookkGothic flex-grow text-[15px]">
-                            <p className="text-[30px] font-bookkMyungjoBold mt-4">Save</p>
-                            <div className={styles.edit_title_container}>
-                                <p style={{ fontSize: "20px" }}>Title</p>
-                                <input
-                                    className={styles.edit_input_box3}
-                                    type="text"
-                                    placeholder="제목을 입력해주세요."
-                                    value={uploadPhotoInfo.title}
-                                    onChange={handleUploadPhotoTitle}
-                                ></input>
-                            </div>
-                            <div className={styles.edit_tag_container}>
-                                <p style={{ fontSize: "20px" }}>Tags</p>
-                                <div className={styles.edit_tag_input_container}>
-                                    <input
-                                        className={styles.edit_input_box4}
-                                        value={inputText}
-                                        type="text"
-                                        placeholder="사진에 태그를 추가해 보세요."
-                                        onChange={(e) => {
-                                            setInputText(e.target.value);
-                                        }}
-                                    ></input>
-                                    <div
-                                        className={styles.edit_plus_btn}
-                                        onClick={() => {
-                                            addTag();
-                                        }}
-                                    >
-                                        <img
-                                            src="/imgs/white_plus.png"
-                                            alt="플러스"
-                                            style={{ width: "22px", height: "22px" }}
-                                        ></img>
+                        <div className="flex flex-col w-full">
+                            {uploadPhoto ? (
+                                <div className="flex flex-col items-center font-bookkGothic flex-grow text-[15px]">
+                                    <p className="text-[30px] font-bookkMyungjoBold mt-4">Save</p>
+                                    <div className={styles.edit_title_container}>
+                                        <p style={{ fontSize: "20px" }}>Title</p>
+                                        <input
+                                            className={styles.edit_input_box3}
+                                            type="text"
+                                            placeholder="제목을 입력해주세요."
+                                            value={uploadPhotoInfo.title}
+                                            onChange={handleUploadPhotoTitle}
+                                        ></input>
                                     </div>
-                                </div>
-                                <div className={styles.edit_tags_container}>
-                                    {tags.map((tag, index) => {
-                                        return (
+                                    <div className={styles.edit_tag_container}>
+                                        <p style={{ fontSize: "20px" }}>Tags</p>
+                                        <div className={styles.edit_tag_input_container}>
+                                            <input
+                                                className={styles.edit_input_box4}
+                                                value={inputText}
+                                                type="text"
+                                                placeholder="사진에 태그를 추가해 보세요."
+                                                onChange={(e) => {
+                                                    setInputText(e.target.value);
+                                                }}
+                                            ></input>
                                             <div
-                                                key={index + "n"}
-                                                className={`${styles.edit_tag} ${index % 2 != 0 ? styles.float : ""}`}
+                                                className={styles.edit_plus_btn}
+                                                onClick={() => {
+                                                    addTag();
+                                                }}
                                             >
-                                                {tag.length > 5 ? "# " + tag.slice(0, 5) + ".." : "# " + tag}
                                                 <img
-                                                    src="/imgs/black_x.png"
-                                                    alt="x"
-                                                    style={{
-                                                        height: "13px",
-                                                        width: "auto",
-                                                        position: "absolute",
-                                                        right: "7px",
-                                                        cursor: "pointer",
-                                                    }}
-                                                    onClick={() => {
-                                                        removeTag(index);
-                                                    }}
+                                                    src="/imgs/white_plus.png"
+                                                    alt="플러스"
+                                                    style={{ width: "22px", height: "22px" }}
                                                 ></img>
                                             </div>
-                                        );
-                                    })}
+                                        </div>
+                                        <div className={styles.edit_tags_container}>
+                                            {tags.map((tag, index) => {
+                                                return (
+                                                    <div
+                                                        key={index + "n"}
+                                                        className={`${styles.edit_tag} ${
+                                                            index % 2 != 0 ? styles.float : ""
+                                                        }`}
+                                                    >
+                                                        {tag.length > 5 ? "# " + tag.slice(0, 5) + ".." : "# " + tag}
+                                                        <img
+                                                            src="/imgs/black_x.png"
+                                                            alt="x"
+                                                            style={{
+                                                                height: "13px",
+                                                                width: "auto",
+                                                                position: "absolute",
+                                                                right: "7px",
+                                                                cursor: "pointer",
+                                                            }}
+                                                            onClick={() => {
+                                                                removeTag(index);
+                                                            }}
+                                                        ></img>
+                                                    </div>
+                                                );
+                                            })}
+                                        </div>
+                                        <div className={styles.edit_btn_container} onClick={handleUploadPhoto}>
+                                            <p className={styles.upload_txt3} style={{ color: "white" }}>
+                                                Upload
+                                            </p>
+                                            <img
+                                                src="/imgs/upload_icon.png"
+                                                alt="아이콘"
+                                                style={{ height: "45px", width: "auto", marginLeft: "10%" }}
+                                            ></img>
+                                        </div>
+                                    </div>
                                 </div>
-                                <div className={styles.edit_btn_container} onClick={handleUploadPhoto}>
-                                    <p className={styles.upload_txt3} style={{ color: "white" }}>
-                                        Upload
-                                    </p>
-                                    <img
-                                        src="/imgs/upload_icon.png"
-                                        alt="아이콘"
-                                        style={{ height: "45px", width: "auto", marginLeft: "10%" }}
-                                    ></img>
+                            ) : (
+                                <div className="flex flex-col items-center font-bookkGothic flex-grow text-[15px]">
+                                    <p className="text-[30px] font-bookkMyungjoBold mt-4">My Setting</p>
+                                    <div className="flex justify-between w-full px-4 pt-8 ">
+                                        <p>조리개 / Aperture </p>
+                                        <p>F {aperture} </p>
+                                    </div>
+                                    <div className="flex justify-between w-full  px-4 pt-5">
+                                        <p>셔터스피드 / SS </p>
+                                        {shutterSpeed === 1 ? <p>1</p> : <p>1/{shutterSpeed}</p>}
+                                    </div>
+                                    <div className="flex justify-between w-full  px-4 pt-5">
+                                        <p>감도 / ISO </p>
+                                        <p>{iso} </p>
+                                    </div>
+                                    <div className="flex justify-between w-full  px-4 pt-5">
+                                        <p>노출 / Exposure </p>
+                                        <p>{exposure} EV</p>
+                                    </div>
+                                    <button
+                                        onClick={uploadHandle}
+                                        className="bg-black text-white font-bookkGothicBold w-44 h-11 rounded-sm mt-auto mb-4"
+                                    >
+                                        SAVE
+                                    </button>
                                 </div>
-                            </div>
+                            )}
                         </div>
-                    ) : (
-                        <div className="flex flex-col items-center font-bookkGothic flex-grow text-[15px]">
-                            <p className="text-[30px] font-bookkMyungjoBold mt-4">My Setting</p>
-                            <div className="flex justify-between w-full px-4 pt-8 ">
-                                <p>조리개 / Aperture </p>
-                                <p>F {aperture} </p>
-                            </div>
-                            <div className="flex justify-between w-full  px-4 pt-5">
-                                <p>셔터스피드 / SS </p>
-                                {shutterSpeed === 1 ? <p>1</p> : <p>1/{shutterSpeed}</p>}
-                            </div>
-                            <div className="flex justify-between w-full  px-4 pt-5">
-                                <p>감도 / ISO </p>
-                                <p>{iso} </p>
-                            </div>
-                            <div className="flex justify-between w-full  px-4 pt-5">
-                                <p>노출 / Exposure </p>
-                                <p>{exposure} EV</p>
-                            </div>
-                            <button
-                                onClick={uploadHandle}
-                                className="bg-black text-white font-bookkGothicBold w-44 h-11 rounded-sm mt-auto mb-4"
-                            >
-                                SAVE
-                            </button>
-                        </div>
-                    )}
-                </div>
-            </div>
+                    </div>
+                </>
+            )}
         </div>
     );
 };
