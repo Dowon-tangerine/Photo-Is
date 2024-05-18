c4d2f3b45f9ff6c51eb60d77b2d8c241439814ff
박창준 <qmak01@naver.com>
2024-05-18 18:17:47 +0900

    [be][fix] createdAt Null 해결
    Former-commit-id: c612da57b88969f1d7cae02f4fdcf7ef3041ee6e

--- backend/src/main/java/org/ssafy/d103/chatbots/dto/ChatMessageDto.java
+++ backend/src/main/java/org/ssafy/d103/chatbots/dto/ChatMessageDto.java
@@ -3,9 +3,12 @@ package org.ssafy.d103.chatbots.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 @AllArgsConstructor
 public class ChatMessageDto {
     private String role;
     private String message;
+    private LocalDateTime createdAt;
 }
