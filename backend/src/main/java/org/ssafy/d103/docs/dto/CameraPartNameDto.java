b3b57379b6aa7b7eede03437d504c2249e354f93
박창준 <qmak01@naver.com>
2024-05-19 01:16:00 +0900

    [be][feat] 카메라 부위 검색 API 추가
    Former-commit-id: 166b0d8828551870804ddad1db81262b65070c7e

--- backend/src/main/java/org/ssafy/d103/docs/dto/CameraPartNameDto.java
+++ backend/src/main/java/org/ssafy/d103/docs/dto/CameraPartNameDto.java
@@ -1,12 +1,14 @@
 package org.ssafy.d103.docs.dto;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class CameraPartNameDto {
     private Long id;
     private String name;
