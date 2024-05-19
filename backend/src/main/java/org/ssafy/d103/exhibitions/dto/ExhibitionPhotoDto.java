e3ffa2073ba2fcb5715ffb6b2f78fe93edca67c7
sehyun <germs1020@gmail.com>
2024-05-19 16:18:24 +0900

    [be][Fix] 전시회 사진 목록 조회시 상세정보도 제공하도록 수정
    Former-commit-id: 7bd200282a50775b5a0a51f69735de4284ffc904

--- backend/src/main/java/org/ssafy/d103/exhibitions/dto/ExhibitionPhotoDto.java
+++ backend/src/main/java/org/ssafy/d103/exhibitions/dto/ExhibitionPhotoDto.java
@@ -4,9 +4,12 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.ssafy.d103.communities.entity.photo.PhotoDetail;
+import org.ssafy.d103.communities.entity.photo.PhotoHashtag;
 import org.ssafy.d103.exhibitions.entity.ExhibitionPhoto;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 @Setter
@@ -18,22 +21,29 @@ public class ExhibitionPhotoDto {
     private String title;
     private String photoUrl;
     private LocalDateTime time;
+    private PhotoDetail photoDetail;
+    private List<PhotoHashtag> hashtag;
 
     @Builder
-    private ExhibitionPhotoDto(Long exhibitionPhotoId, int number, String title, String photoUrl, LocalDateTime time) {
+    private ExhibitionPhotoDto(Long exhibitionPhotoId, int number, String title, String photoUrl, LocalDateTime time, PhotoDetail photoDetail, List<PhotoHashtag> hashtag) {
         this.exhibitionPhotoId = exhibitionPhotoId;
         this.number = number;
         this.title = title;
         this.photoUrl = photoUrl;
         this.time = time;
+        this.photoDetail = photoDetail;
+        this.hashtag = hashtag;
     }
 
     public static ExhibitionPhotoDto from(ExhibitionPhoto exhibitionPhoto) {
         return builder()
                 .exhibitionPhotoId(exhibitionPhoto.getExhibitionId().getId())
                 .number(exhibitionPhoto.getNumber())
+                .title(exhibitionPhoto.getPhotoId().getTitle())
                 .time(exhibitionPhoto.getPhotoId().getCreatedAt())
                 .photoUrl(exhibitionPhoto.getPhotoId().getImageUrl())
+                .photoDetail(exhibitionPhoto.getPhotoId().getPhotoDetail())
+                .hashtag(exhibitionPhoto.getPhotoId().getPhotoHashtagList())
                 .build();
     }
 }
