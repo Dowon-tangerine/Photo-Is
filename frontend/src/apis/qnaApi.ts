73c052c516dbd39e0dda5730aa333318766efb2a
rlarbfl0702 <rlarbfl0702@naver.com>
2024-05-20 04:41:26 +0900

    [Fe][Feat] api 연결 1. 게시글 작성 연결 2. 카테고리 구분하고 사진 유무 모두 업로드 가능
    Former-commit-id: c6bc64d4e2f285f07b0675354f90a289e15c1147 [formerly df9fdc17d460b07ca84f66957128b43eddd61a24]
    Former-commit-id: 671d028513c43036169763f69160052e5e97a6b6

--- frontend/src/apis/qnaApi.ts
+++ frontend/src/apis/qnaApi.ts
@@ -28,3 +28,47 @@ export const getQnaSortList = async (category : string, page : number) => {
         return false;
     })
 }
+
+export const postQna = async (photoId : number | null, category : string, title : string, content : string) => {
+    const url = `/questions/upload`
+
+    const data = {
+        photoId: photoId === null ? "" : photoId, category: category, title: title, content: content
+    }
+    return await instance.post(url, data)
+    .then(res => {
+        console.log(photoId)
+        console.log("글업로드" + res)
+        return res.data.data;    
+    })
+    .catch(error => {
+        console.log("글업로드" + error);
+        return false;
+    })
+}
+
+export const postPhoto = async (사진 : File, title : string, accessType : string, hashtag: Array<string> ) => {
+	const url = `/photos/upload`;
+
+    const formData = new FormData();
+    formData.append('photo', 사진);
+
+    const photoInfo = JSON.stringify({
+        title: title,
+        accessType: accessType,
+        hashtagList: hashtag
+    });
+    const blob = new Blob([photoInfo], {type: "application/json"});
+    formData.append("photoInfo", blob);
+
+	return await instance.post(url, formData)
+    .then(res => {
+        // console.log(res)
+        return res.data.data;    
+    })
+    .catch(error => {
+        console.log(error);
+        return false;
+    })
+}
+
